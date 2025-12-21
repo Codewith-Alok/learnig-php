8052dcc7608a28debb1521b6e3addaced4a3d5a9
Alok Raj <alokraj32135@gmail.com>
2025-12-21 14:53:24 +0530

    Add PHP script to greet user with HTML

---
+++ phpwithhtml.php
@@ -0,0 +1,8 @@
+<?php
+$name = "John Doe";
+echo "Hello, " . $name . "!";
+echo "<h1 style='color: rgba(132, 31, 31, 1);'>" . $name . "</h1>";
+?>
+<br>
+
+<h2>Welcome to my website, <?php echo $name; ?>!</h2>
